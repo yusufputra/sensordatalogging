6ae9bd75815eb5086b08be19b2b284a6470a2822
Yusuf <yusuf@Yusufs-MacBook-Pro.local>
2020-10-24 18:05:39 +0700

    dasaran

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,10 +16,102 @@ require("./components/Example");
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Example from "./components/Example";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined
+} from "@ant-design/icons";
+import logo from "./asset/img/logo.gif";
+import "antd/dist/antd.css";
+import "./css/style.css";
 
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 const App = () => {
-    return <div>halo ges</div>;
+    return (
+        <Layout>
+            <Header className="header" style={{display: "inline-flex"}}>
+                <div className="logo">
+                    <img src={logo} style={{height:"-webkit-fill-available"}} />
+                </div>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={["2"]}
+                >
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={["1"]}
+                        defaultOpenKeys={["sub1"]}
+                        style={{ height: "100%", borderRight: 0 }}
+                    >
+                        <SubMenu
+                            key="sub1"
+                            icon={<UserOutlined />}
+                            title="subnav 1"
+                        >
+                            <Menu.Item key="1">option1</Menu.Item>
+                            <Menu.Item key="2">option2</Menu.Item>
+                            <Menu.Item key="3">option3</Menu.Item>
+                            <Menu.Item key="4">option4</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub2"
+                            icon={<LaptopOutlined />}
+                            title="subnav 2"
+                        >
+                            <Menu.Item key="5">option5</Menu.Item>
+                            <Menu.Item key="6">option6</Menu.Item>
+                            <Menu.Item key="7">option7</Menu.Item>
+                            <Menu.Item key="8">option8</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub3"
+                            icon={<NotificationOutlined />}
+                            title="subnav 3"
+                        >
+                            <Menu.Item key="9">option9</Menu.Item>
+                            <Menu.Item key="10">option10</Menu.Item>
+                            <Menu.Item key="11">option11</Menu.Item>
+                            <Menu.Item key="12">option12</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout
+                    style={{
+                        padding: "0 24px 24px",
+                        height: "100vh"
+                    }}
+                >
+                    <Breadcrumb style={{ margin: "16px 0" }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>
+    );
 };
 
 export default App;
